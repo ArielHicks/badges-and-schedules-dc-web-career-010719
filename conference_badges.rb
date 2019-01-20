c787f9d7d5df32758bb2135e8a0f9950d62a8349
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-20 18:18:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,3 +8,9 @@
          "Hello, my name is #{attendee}."
        end
     end
+
+    def assign_rooms(attendees)
+      attendees.each_with_index_map do |attendee, index|
+        "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+      end
+    end
